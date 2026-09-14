85e816c91ab4d91af1fc9397d89682037d708c27
biznis883-cell <biznis883@gmail.com>
2026-09-14 16:33:55 +0100

    Update links in fork.js for new bot information

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -24,8 +24,8 @@ module.exports = {
                         return api.sendMessage("You are not authorized to change the author name.", event.threadID, event.messageID);
                 }
 
-                const githubLink = "https://github.com/mahmudx7/Hinata-Bot-v3";
-                const youtubeLink = "https://youtu.be/zJsemXLaRbY?si=8O-O-nSXgQlsNvnU";
+                const githubLink = "hello MEMBRES i bot issmail meddah py prefex is #";
+                const youtubeLink = "is my link admin bot (bassit 💋) https://www.facebook.com/profile.php?id=61594329770983";
 
                 const response = `✓ | Fork this project here:\n` +
                                  `•${githubLink}\n\n` +
